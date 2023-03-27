ab00b9f166e6c31ae376d658614f9f04c88ebd48
theophilus doku <theodoku89@gmail.com>
2023-03-27 14:34:39 +0000

    Added react checkbox fields

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import TodosContainer from './components/TodosContainer';
 
+ function App () {
+  return ( 
+    <TodosContainer/>
+   );
+}
+ 
+export default App;
---
+++ src/components/TodosContainer.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import Header from './Header';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+
+const TodosContainer = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ]);
+    
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+    };
+    
+    const addTodoItem = title => {
+        const newTodo = {
+            id: 4,
+            title: title,
+            completed: false,
+        };
+        setTodos([...todos, newTodo])
+    };
+
+  return (
+      <div>
+          <Header />
+          <TodoInput addTodoProps={addTodoItem} />
+          <TodoList todos={todos} handleChangeProps={handleChange}/>
+    </div>
+  );
+};
+export default TodosContainer;

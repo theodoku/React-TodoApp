a029df20617044dfe035723a8734a359ec146dd1
theophilus doku <theodoku89@gmail.com>
2023-03-27 16:31:11 +0000

    Modified App.js, index.js and added index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import TodosContainer from './components/TodosContainer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <TodosContainer />
-  </React.StrictMode>,
-);
+function App() {
+  return (
+    <>
+      <TodosContainer />
+    </>
+  );
+}
+
+export default App;
